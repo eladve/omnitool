cf42258d8a567b3fc4fbfd480b81e03c7326f1eb
Elad Verbin <elad.verbin@gmail.com>
2020-05-05 15:49:20 +0200

    Update omnitool_helper_functions.py
    1. helper function
    2. unicode support

--- omnitool_helper_functions.py
+++ omnitool_helper_functions.py
@@ -1,3 +1,5 @@
+# coding=UTF-8
+
 from PIL import Image
 
 def canonicise_word(w):
@@ -5,14 +7,22 @@ def canonicise_word(w):
 # TODO: to enable good search quality, need to make this better, e.g. by using maybe using some NLP library to kern the word, etc
 # also, I think the whole program doesn't work propertly on non-latin text; maybe even not on non-english text.
     try:
-        w = str(w).lower()
-        w = w.strip("?-><:!@#$%^&*()""\\''.,`~][{}]")
+        w = w.lower()
+        w = w.strip(u"?-><:!=+-|_/:;“”@#$%^&*()\"\\\'.,`~][{}]")
+        w = w.encode('UTF-8')
         if len(w) <= 1:
             return ""
         else:
             return w
     except UnicodeEncodeError:
         return ""
+
+def display_screenshot_by_filename(filename):
+    screenshot = Image.open(filename)
+    try: # works in ipython notebook
+        display(screenshot)
+    except:
+        screenshot.show()
 
 def compress_screenshot_and_save(screenshot, filename):
     compressed_screenshot = screenshot.copy()
